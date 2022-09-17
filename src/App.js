bb8e7e2c3162e6db9e814473956ddd9419c91df9
Piyush Yadav <85944872+piy-crpto@users.noreply.github.com>
2022-09-17 12:33:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Register from './pages/register/Register';
+// import Register from './pages/register/Register';
 import Home from './pages/home/Home';
 //import Profile from './pages/profile/Profile';
 
